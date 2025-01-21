a9798189ac1d6327c003ace5e2abf00f8099234b
Yan Cauê <127339610+YanzinhoCaue@users.noreply.github.com>
2025-01-21 10:46:42 +0000

    Desafio Mestre feito com Sucesso

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int zipcode, population, age, touristAttractions;
+    char cardCode[3];
+    int zipcode, population, age, touristAttractions, cityOneWins;
     char cityName[50];
-    float area, gdp, populationDensity, gdpPerCapita;
+    float area, gdp, populationDensity, gdpPerCapita, calculateMedia;
+
+    char cardCodeDois[3];
+    int zipcodeDois, populationDois, ageDois, touristAttractionsDois, cityTwoWins;
+    char cityNameDois[50];
+    float areaDois, gdpDois, populationDensityDois, gdpPerCapitaDois, calculateMediaDois;
 
     //Introdução ao jogo
     printf("================================\n");
@@ -13,6 +19,9 @@ int main() {
     //Cadastro das informações das cidades
 
     //Cadastrando o cep da cidade
+    printf("Digite o codigo da carta:\n");
+    scanf("%d", &cardCode);
+
     printf("Digite o cep da cidade:\n");
     scanf("%d", &zipcode);
 
@@ -39,7 +48,41 @@ int main() {
 
     //Confirmação de Cadastro
     printf("=======CARTA REGISTRADA COM SUCESSO========\n");
+
     printf("CEP: %d\n" "CIDADE: %s\n" "IDADE: %d\n" "AREA: %f\n" "PONTOS TURISTICOS: %d\n" "PIB: %f\n", zipcode, cityName, age, area, touristAttractions, gdp);
+
+    //Cadastrando o cep da cidade
+    printf("Digite o codigo da carta:\n");
+    scanf("%d", &cardCodeDois);
+
+    printf("Digite o cep da cidade:\n");
+    scanf("%d", &zipcodeDois);
+
+
+    //Cadastrando o nome da cidade
+    printf("Digite o nome da cidade:\n");
+    scanf("%s", &cityNameDois);
+
+    //Cadastrando a idade da cidade
+    printf("Digite a idade da cidade:\n");
+    scanf("%d", &ageDois);
+
+    //Cadastrando a area geografica da cidade
+    printf("Digite a area geografica da cidade:\n");
+    scanf("%f", &areaDois);
+
+    //Cadastrando o número de pontos turisticos
+    printf("Digite o número de Pontos Turisticos:\n");
+    scanf("%d", &touristAttractionsDois);
+
+    //Cadastrando o PIB da cidade
+    printf("Digite o PIB da cidade:\n");
+    scanf("%f", &gdpDois);
+
+    //Confirmação de Cadastro
+    printf("=======CARTA REGISTRADA COM SUCESSO========\n");
+
+    printf("CEP: %d\n" "CIDADE: %s\n" "IDADE: %d\n" "AREA: %f\n" "PONTOS TURISTICOS: %d\n" "PIB: %f\n", zipcodeDois, cityNameDois, ageDois, areaDois, touristAttractionsDois, gdpDois);
 
     //Calculo Nivel Aventureiro
     //Calculando a Densidade Populacional
@@ -54,8 +97,37 @@ int main() {
     gdpPerCapita = gdp / population;
     printf("PIB PER CAPITA: %f\n", gdpPerCapita);
 
+    //Calculando a Densidade Populacional
+    printf("\n\n================================\n");
+    printf("CALCULANDO DENSIDADE POPULACIONAL CIDADE DOIS\n");
+    populationDensity = populationDois / areaDois;
+    printf("DENSIDADE POPULACIONAL: %f\n", populationDensityDois);
+    
+    //Calculando PIB per Capita
+    printf("\n\n================================\n");
+    printf("CALCULANDO PIB PER CAPITA CIDADE DOIS\n");
+    gdpPerCapita = gdpDois / populationDois;
+    printf("PIB PER CAPITA: %f\n", gdpPerCapitaDois);
+
+    //Comparação Nivel Mestre
+    printf("\n\n================================\n");
+    printf("COMPARANDO CARTAS\n");
+
+    calculateMedia = zipcode + population + age + touristAttractions + area + gdp / 6;
+    calculateMediaDois = zipcodeDois + populationDois + ageDois + touristAttractionsDois + areaDois + gdpDois / 6;
+
+    printf("A media das metricas da cidade 1 é de: %.1f\n", calculateMedia);
+    printf("A media das metricas da cidade 2 é de: %.1f\n", calculateMediaDois);
+
+    cityOneWins = calculateMedia > calculateMediaDois;
+    cityTwoWins = calculateMedia < calculateMediaDois;
+
+
+    printf("A cidade 1 venceu! %f", cityOneWins);
+    printf("A cidade 2 venceu! %f", cityTwoWins);
     //Desafio para Novato completado com sucesso.
     //Desafio para Aventureiro completado com sucesso.
+    //Desafio para Mestre completado com sucesso.
 
     return 0;
 }
